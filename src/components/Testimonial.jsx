8e5651d7fa4f11fa1bb0630f11fcbff15c12c074
IRASUBIZA-SALY-NELSON <irasubizasalynelson@gmail.com>
2025-12-02 11:07:20 +0200

    solved imagekit

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -6,18 +6,18 @@ import { motion } from 'motion/react';
 const Testimonial = () => {
 
     const testimonials = [
-        { name: "Emma Rodriguez", 
-          location: "Barcelona, Spain", 
+        { name: "Emmanuel Rodrigue", 
+          location: "Kigali, Rwanda", 
           image: assets.testimonial_image_1, 
           testimonial: "I've rented cars from various companies, but the experience with CarRental was exceptional." 
         },
-        { name: "John Smith", 
-          location: "New York, USA", 
+        { name: "Iranzi Samuella", 
+          location: "Nyagatare, Rwanda", 
           image: assets.testimonial_image_2, 
           testimonial: "CarRental made my trip so much easier. The car was delivered right to my door, and the customer service was fantastic!" 
         },
-        { name: "Ava Johnson", 
-          location: "Sydney, Australia", 
+        { name: "Nelson Salim", 
+          location: "Kigali, Rwanda", 
           image: assets.testimonial_image_1, 
           testimonial: "I highly recommend CarRental! Their fleet is amazing, and I always feel like I'm getting the best deal with excellent service." 
         }

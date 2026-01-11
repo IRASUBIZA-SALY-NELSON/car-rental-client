a52e3af2d3b0b41c8a5bbd1b0e7f993ba7b668a8
IRASUBIZA-SALY-NELSON <irasubizasalynelson@gmail.com>
2026-01-11 10:32:10 +0200

    done , and remainign task is for image handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
 import Testimonial from '../components/Testimonial'
 import Newsletter from '../components/Newsletter'
+import LoadingScreen from '../components/LoadingScreen'
 
 const Home = () => {
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 2000);
+    return ()=> clearTimeout(timer)
+  },[])
+
+  if(loading){
+    return <LoadingScreen />
+  }
+
   return (
     <>
       <Hero />
